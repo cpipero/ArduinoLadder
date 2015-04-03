f4f7b5b58ac822877a8ebd8d4be412485837faf4
epirogov <epirogov@windowslive.com>
2015-04-03 16:37:54 +0300

    arduino IDE binaries

---
+++ Libs/arduino-1.6.3-windows/arduino-1.6.3/reference/arduino.cc/en/Reference/arduino.cc
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.48-19 [XR&CO'2014] -->
+
+<!-- Mirrored from arduino.cc/en/Reference/arduino.cc by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Feb 2015 20:13:32 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://arduino.cc/en/Arduino/Cc"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://arduino.cc/en/Arduino/Cc"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.48-19 [XR&CO'2014] -->
+
+<!-- Mirrored from arduino.cc/en/Reference/arduino.cc by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Feb 2015 20:13:32 GMT -->
+</HTML>
